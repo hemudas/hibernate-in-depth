ed95d32a6e11ac8b62a5c63f2ab9e8459389b7fc
hemdas <hemdas@WKWIN2450788.global.publicisgroupe.net>
2021-07-19 17:17:31 +0530

    Basic setup is done.

---
+++ hibernate-in-depth/src/test/java/in/hid/app/HibernateInDepthApplicationTests.java
@@ -0,0 +1,13 @@
+package in.hid.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateInDepthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
